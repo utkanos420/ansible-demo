77e7851c91e9b2e9cda3b0d003baeaeb1d165ad6
Maxim <daseoffc@gmail.com>
2025-12-28 20:49:23 +0000

    feat: add command aliases in script.sh to simplify actions

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# ssh-agent runs in background to enter passphrases all time
 eval $(ssh-agent)
 ssh-add /home/ubuntu/.ssh/ansible
 ssh-add /home/ubuntu/.ssh/id_ed25519
+
+# aliases to simplify actions
+alias ansiblepc="ansible-playbook -i inventories/pc/inventory"
+alias ansiblelaptop="ansible-playbook -i inventories/laptop/inventory"
